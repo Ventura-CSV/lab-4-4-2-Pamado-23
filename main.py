b7e06881c43d8a58d44824509c160c53138488eb
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-28 02:30:32 -0700

    Lab 4-4-2

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def main():
     ########################################
     """
 
+    while True:
+        try:
+            number = int(input(f'Enter a number'))
+        except ValueError:
+            print (f'Invalid input: Value Error')
+            continue
+        else:
+            print (number)
+            break
+
+
     ########################################
     # Do not delete the return statement
     ########################################
